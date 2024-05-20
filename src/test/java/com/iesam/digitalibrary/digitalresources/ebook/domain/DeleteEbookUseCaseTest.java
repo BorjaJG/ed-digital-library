2930a1f3be603b1d63534cf0346e55fd04127b7c
Borja <borjajg02@gmail.com>
2024-05-20 17:48:55 +0200

    Test:Testing Outcomes for DeleteEbookUseCase Module
     ->he "Testing Outcomes for DeleteEbookUseCase Module" is a document that details the results obtained from evaluating the use case named "DeleteEbookUseCase."

--- src/test/java/com/iesam/digitalibrary/digitalresources/ebook/domain/DeleteEbookUseCaseTest.java
+++ src/test/java/com/iesam/digitalibrary/digitalresources/ebook/domain/DeleteEbookUseCaseTest.java
@@ -1,6 +1,10 @@
 package com.iesam.digitalibrary.digitalresources.ebook.domain;
 
+import static org.junit.jupiter.api.Assertions.*;
 
+import com.iesam.digitalibrary.loan.domain.LoanRepository;
+import com.iesam.digitalibrary.user.domain.DeleteUserUseCase;
+import com.iesam.digitalibrary.user.domain.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -9,33 +13,42 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+
 @ExtendWith(MockitoExtension.class)
-class ModifyEbookUseCaseTest {
+class DeleteEbookUseCaseTest {
+
     @Mock
     EbookRepository ebookRepository;
 
-    ModifyEbookUseCase modifyEbookUseCase;
+    DeleteEbookUseCase deleteEbookUseCase;
+
 
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.openMocks(this);
-        modifyEbookUseCase = new ModifyEbookUseCase(ebookRepository);
+        deleteEbookUseCase = new DeleteEbookUseCase(ebookRepository);
     }
 
     @AfterEach
     public void tearDown() {
-        modifyEbookUseCase = null;
+        deleteEbookUseCase = null;
     }
+
 
     @Test
-    public void testModifyEbook() {
+    public void testDeleteUser() {
         // Given
-        Ebook ebook = new Ebook("1", "1", "1", "1", "1");
+        String isbn = "1";
 
         // When
-        modifyEbookUseCase.execute(ebook);
+        deleteEbookUseCase.execute(isbn);
 
         // Then
-        Mockito.verify(ebookRepository, Mockito.times(1)).modify(ebook);
+        Mockito.verify(ebookRepository, Mockito.times(1)).delete(isbn);
     }
+
+
+
+
 }
