8ddcfbd9f5a46aac1168bbce4cd4ba1b2c5d442b
Borja <borjajg02@gmail.com>
2024-05-20 13:10:05 +0200

    Test: Testing Outcomes for deleteUserUseCase Module #36
     ->The "Testing Outcomes for deleteUserUseCase Module" is a document that details the results obtained from evaluating the use case named "deleteUserUseCase."

--- src/test/java/com/iesam/digitalibrary/user/domain/ModifyUserUseCaseTest.java
+++ src/test/java/com/iesam/digitalibrary/user/domain/ModifyUserUseCaseTest.java
@@ -6,37 +6,43 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 @ExtendWith(MockitoExtension.class)
-class NewUserUseCaseTest {
-
+class ModifyUserUseCaseTest {
     @Mock
     UserRepository userRepository;
-    NewUserUseCase newUserUseCase;
+
+    DeleteUserUseCase deleteUserUseCase;
 
     @BeforeEach
     public void setUp() {
-        newUserUseCase = new NewUserUseCase(userRepository);
+        MockitoAnnotations.openMocks(this);
+        deleteUserUseCase = new DeleteUserUseCase(userRepository);
     }
 
     @AfterEach
-    public void clear() {
-        newUserUseCase = null;
+    public void tearDown() {
+        deleteUserUseCase = null;
     }
 
     @Test
-    public void GetUserAndSaveUser() {
-        //Given
-        User user = new User("1", "2", "juan", "amigo@amigo", "617929803", "1", "1",
-                "1", "1", "1", "1", "1", "1", "1");
-        //When
-        newUserUseCase.execute(user);
-        //Then
-        Mockito.verify(userRepository, Mockito.times(1)).save(user);
+    public void testDeleteUser() {
+        // Given
+        String userId = "1";
 
+        // When
+        deleteUserUseCase.execute(userId);
+
+        // Then
+        Mockito.verify(userRepository, Mockito.times(1)).delete(userId);
     }
+
+
+
+
+
 
 }
